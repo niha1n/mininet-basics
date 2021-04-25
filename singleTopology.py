d277398b2f18e58331237e5e5ae269d552f4446e
Nihal N <81716192+niha1n@users.noreply.github.com>
2021-04-25 09:15:47 +0530

    Update and rename ex1.py to singleTopology.py

--- singleTopology.py
+++ singleTopology.py
@@ -27,16 +27,16 @@ def perfTest():
     topo = SingleSwitchTopo(n=4)
     net = Mininet(topo=topo,host=CPULimitedHost, link=TCLink)
     net.start()
-    print ("Dumping host connections")
+    print "Dumping host connections"
     # To Do 1: Dump hosts connections here
     dumpNodeConnections(net.hosts)
-    print ("Testing network connectivity")
+    print "Testing network connectivity"
     # To Do 2: Ping all hosts via single command
     net.pingAll()
-    print ("Testing bandwidth between h1 and h3")
+    print "Testing bandwidth between h1 and h3"
     # To Do 3: Test bandwidth between host 1 & host 3 
     # HINT: First get the names of hosts from network then iperf them.
-    h1, h2 = get('h1','h2')
+    h1, h2 = net.get('h1','h2')
     net.iperf((h1,h2))
     net.stop()
  
